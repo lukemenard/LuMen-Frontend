0551b67c48b6dd2b4ca4be9ff6a0e4738fdc02a9
lukemenard <lukemenard@gmail.com>
2019-10-07 13:15:25 -0600

    clean

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
 import './App.css';
 import NavContainer from './components/navbar/NavContainer'
 import PortfolioContainer from './components/portfolio/PortfolioContainer'
-import ExpertiseContainer from './components/expertise/ExpertiseContainer'
 import ExperienceContainer from './components/experience/ExperienceContainer'
 import ContactContainer from './components/contact/ContactContainer'
 import Home from './components/home/Home'
@@ -20,7 +19,6 @@ const App = (props) => {
         <div className="Routes">
           <Route exact path="/" component={Home} />
           <Route exact path='/experience' component={ExperienceContainer} />
-          <Route exact path='/expertise' component={ExpertiseContainer} />
           <Route exact path="/portfolio" component={PortfolioContainer} />
           <Route exact path="/contact" component={ContactContainer} />
         </div>
